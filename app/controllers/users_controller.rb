93fe59af16d3976aacc74dded4637a0e7114f2e0
Salsa <tiago.salsa.doce@gmail.com>
2015-06-22 20:19:55 +0100

    Added people filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,17 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:show]
+  before_action :authenticate_user!, only: [:index]
 
 	def index
-		@users = User.all
+		case params[:people] when "friends"
+      @users = current_user.active_friends
+    when "requests"
+      @users = current_user.pending_friend_requests_from
+    when "pending"
+      @users = current_user.pending_friend_requests_to
+    else
+      @users = User.where.not(id: current_user.id)
+    end
 	end
 
   def show
